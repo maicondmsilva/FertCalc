9eba59589a6aa894286ab7f01f125e523f92cc3a
maicondmsilva <maicondmsilva@gmail.com>
2026-03-19 12:25:59 -0300

    FASE 1: Add custom hooks and core types/constants

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export { default as useCalculatorFormulas } from './useCalculatorFormulas';
+export { default as useMaterialManagement } from './useMaterialManagement';
+export { default as usePricingFactors } from './usePricingFactors';
